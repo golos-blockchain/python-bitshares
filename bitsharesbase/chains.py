2934ac8b30455102b186cf4e8f4d2ff0f0766d57
1aerostorm <aerostorm1@gmail.com>
2022-10-07 19:52:04 +0000

    Add GOLOS chain_id

--- bitsharesbase/chains.py
+++ bitsharesbase/chains.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 known_chains = {
     "GPH": {
-        "chain_id": "7fcf452d6bb058949cdc875b13c8908c8f54b0f264c39faf8152b682af0740ee",
-        "core_symbol": "GPH",
-        "prefix": "GPH",
+        "chain_id": "782a3039b478c839e4cb0c941ff4eaeb7df40bdd68bd441afd444b9da763de12",
+        "core_symbol": "GLS",
+        "prefix": "GLS",
     },
     "CTS": {
         "chain_id": "53952fc10d52a3af6d572a298e8f99a0a2e7afd0f7dbad354d8d65e8c36c962d",
